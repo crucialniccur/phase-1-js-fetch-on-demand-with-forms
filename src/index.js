ff27c9d686bf446d8b70d0fc97a0d40029f3eb92
Mati Joseph <crucialniccur@gmail.com>
2025-03-20 05:11:28 +0300

    access dom elements in the fetch

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const init = () => {
     event.preventDefault();
     // console.log(event.target.children[1].value);
     const input = document.querySelector("input#searchByID");
-    console.log(input.value);
+    // console.log(input.value);
 
     fetch(`http://localhost:3001/movies/${input.value}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
       });
   });
 };
