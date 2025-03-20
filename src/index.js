3da6d53d225a2e237bd03798580afb9ba80e5039
Mati Joseph <crucialniccur@gmail.com>
2025-03-20 04:55:44 +0300

    grab the converted data in a second then and log it to the console

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const init = () => {
     const input = document.querySelector("#searchByID");
     console.log(input.value);
 
-    fetch("http://localhost:3001/movies").then((response) => response.json());
+    fetch("http://localhost:3001/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   });
 };
 
