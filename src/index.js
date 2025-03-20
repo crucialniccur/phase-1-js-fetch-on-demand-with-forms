59e0bf664f46eaa58851ba2db7156d33ac6ca94e
Mati Joseph <crucialniccur@gmail.com>
2025-03-20 04:54:41 +0300

    use the fetch and .json to call and convert the data to javaScript object

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const init = () => {
     // console.log(event.target.children[1].value);
     const input = document.querySelector("#searchByID");
     console.log(input.value);
+
+    fetch("http://localhost:3001/movies").then((response) => response.json());
   });
 };
 
